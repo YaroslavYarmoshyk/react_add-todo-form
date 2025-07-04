7f0d547f1f5d2ab3c13a1c0567d599de996e986c
Yarmoshyk <yarmoshyk96@gmail.com>
2025-07-04 11:02:12 +0300

    Add fixes after second review

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,12 +1,6 @@
 import { User } from '../types/User';
 import usersFromServer from '../api/users';
 
-export function getUserById(id: number): User {
-  const foundUser = usersFromServer.find((user: User) => user.id === id);
-
-  if (!foundUser) {
-    throw new Error('User not found by id: ' + id);
-  }
-
-  return foundUser;
+export function getUserById(id: number): User | undefined {
+  return usersFromServer.find((user: User) => user.id === id);
 }
